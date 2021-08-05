85325f9b8715c4ea994e0230a29f000a02e7f9f2
Oyatunji Adedamola <dedribble@gmail.com>
2021-08-05 22:04:37 +0100

    A simple and functional toggle button

---
+++ app.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector('.toggle-box');
+const whole = document.querySelector('.whole');
+
+    toggle.addEventListener('click', ()=>{
+        whole.classList.toggle('active')
+    })
